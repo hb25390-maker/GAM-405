0fce5422917e16247cb582c5ca7ef5ae0e360662
SUCART777 <sc4778@my.acm.ac.uk>
2025-10-15 16:32:26 +0100

    initialise

---
+++ Gam 405 3D/Assets/Input/PlayerMovement.cs
@@ -0,0 +1,52 @@
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
+public class Playermovement : MonoBehaviour
+{
+
+    public InputActionReference moveAction;
+
+    public float moveSpeed = 5f;
+
+    [SerializeField] private Vector2 moveInput;
+
+    [SerializeField] private Rigidbody rb;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //this grabs the rigid body component 
+
+    }
+    void Update()
+    {
+        moveInput = moveAction != null ? moveAction.action.ReadValue<Vector2>() : Vector2.zero;
+
+    }
+
+    private void FixedUpdate()
+    {
+        HandleMovement();
+    }
+
+    void OnEnable()
+    {
+        moveAction?.action.Enable();
+
+    }
+    void OnDisable()
+    {
+        moveAction?.action.Disable();
+
+    }
+
+    private void HandleMovement()
+    {
+        Vector3 inputDir = new Vector3(moveInput.x, 0f, moveInput.y);
+        if (inputDir.sqrMagnitude > 1f) inputDir.Normalize();
+
+    }
+}
